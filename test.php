8aa98cd29f680d387ee81dc8d6e030f4f08a8fe7
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-08-05 15:09:01 +0800

    test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 	mysql_connect("localhost","root","1234");//最後一欄要輸入密碼
-	mysql_select_db("guest");//guest為資料庫名稱
+	mysql_select_db("Service_deskHW3");//guest為資料庫名稱
 	mysql_query("set names utf8");//將資料設為utf8格式（才能讀取中文）
 ?>
 <!DOCTYPE html>
