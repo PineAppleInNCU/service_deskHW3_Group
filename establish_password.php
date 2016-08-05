7017b42ea23c4108e766cc4f1266b0b23a9ce520
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-08-05 21:03:46 +0800

    login

--- establish_password.php
+++ establish_password.php
@@ -13,8 +13,8 @@
 
   if(isset($_POST['username'])){
 
-    mysql_query("insert into admin value('$username','$password','$email')  ");
-    header("location:login.php");
+    mysql_query("insert into admin value('$username','$password')  ");//let username and password store in database
+    header("location:login.php");//頁面跳轉
 
   }
 
