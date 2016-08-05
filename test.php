ff5fd0a3d50c358d15a9c64c585440519a9fb471
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-08-05 18:47:13 +0800

    fix the test.php

--- test.php
+++ test.php
@@ -1,16 +1,23 @@
 <?php
-	mysql_connect("localhost","root","tommy522588");//最後一欄要輸入密碼
-	mysql_select_db("Service_deskHW3");//guest為資料庫名稱
-	mysql_query("set names utf8");//將資料設為utf8格式（才能讀取中文）
+
+  //一、連結資料庫
+  //建立連線
+  $link = mysql_connect("localhost", "root", "tommy522588");
+  //設定語系
+  mysql_query("SET NAMES 'UTF-8'");
+  //選擇資料庫
+  mysql_select_db("Service_deskHW3") or die("無法選擇資料庫");
+
+
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	</head>
-	<body class="bg-info">
-		<?php
-			echo "Hello world!";
-		?>
-	</body>
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        </head>
+        <body class="bg-info">
+                <?php
+                        echo "Hello world!";
+                ?>
+        </body>
 </html>
