9a729d3617302e6ca74f7e0d0d2a9597318edd25
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-08-05 14:54:46 +0800

    test the website link

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+	mysql_connect("localhost","root","tommy522588");//最後一欄要輸入密碼
+	mysql_select_db("test");
+	mysql_query("set names utf8");
+
+	$data=mysql_query('select * from test order by number desc');//從guest_2抓取資料
+	echo $data;
+
+	echo "<br>";
+
+	$total=mysql_num_rows($data);
+	echo $total;
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+</head>
+<body class="bg-info">
+	<?php
+		echo "Hello world!";
+	?>
+
+
+</body>
+</html>
