b7afa661523bc1d554209f39e8c360e12d75f0d7
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-08-06 10:54:31 +0800

    board Message board

--- board.php
+++ board.php
@@ -12,8 +12,9 @@
   {
     if($_GET['msg']=="logout")
     {
-      echo "logout!!";
-      $_SESSION['v']='';//點下logout鈕，將會把session變成空值
+      
+      $_SESSION['v']='logout~!';//點下logout鈕，將會把session變成空值
+      echo $_SESSION['v'];
       header("location:login.php");
     }
   }
@@ -26,9 +27,33 @@
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         </head>
         <body class="bg-info">
-                <?php
-                        echo "Hello world!";
-                ?>
+          <?php
+            echo "Hello world!";
+          ?>
+          <!-- 輸入留言 -->
+          <?php if(isset($_SESSION['v']))if($_SESSION['v']=="yes"){ ?>
+
+            <div><?php //確定是使用者，才顯示這一個區塊?>
+              <P><font><?php echo "$guestName" ?></font>來說點甚麼吧</P>
+              <div>
+                <form id="form1" name="form1" method="post" action="">
+                  <textarea class="post" style="width:600px;color:white" rows="8" id="guestContent" name="guestContent"></textarea>
+                  <?php echo "</br>"?>
+                  <input type="submit" name="button" id="button" value="確認" />
+                </form>
+                <div>
+                  <a href="php_newboard.php?msg=logout" style="text-decoration:none;color: #3D59AB;font-family: Verdana, sans-serif;"><span style="font-weight: bold;font-size: 18px;color: #ffffff;">登出</span></a>
+                </div>
+                <div >
+                  <a href="php_adminfix.php"><span>帳密修改</span></a>
+                </div>
+              </div>
+            </div>
+            <!-- 輸入留言 -->
+          <?php }?>
+
+
+
 
                 <a href="board.php?msg=logout"><button>登出</button></a>
 
